df78f19edc42e36550f93e035b93679fb05c946b
adiallo21 <abdoulaye32397@gmail.com>
2022-09-05 16:36:27 +0100

    remove console.log

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const { getTopics } = require("./controllers/topics.controller");
 app.get("/api/topics", getTopics);
 
 app.use((err, req, res, next) => {
-  console.log(err, "<--- ERROR");
   res.status(500).send({ msg: "Internal server error" });
 });
 
