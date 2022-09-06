5106dab0b46bd67aff642f80e3849f1d8313a655
adiallo21 <abdoulaye32397@gmail.com>
2022-09-06 13:29:39 +0100

    add get article by id endpoint

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 
 const { getTopics } = require("./controllers/topics.controller");
+const { getArticleById } = require("./controllers/articles.controller");
 
 app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticleById);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ msg: "Internal server error" });
