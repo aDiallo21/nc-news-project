208600450574a106302d0502aec6fdc10e244c2e
adiallo21 <abdoulaye32397@gmail.com>
2022-09-06 14:51:11 +0100

    add error handling

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,7 +1,11 @@
 const { collectTopics } = require("../models/topics.model");
 
 exports.getTopics = (req, res, next) => {
-  collectTopics().then((topics) => {
-    res.status(200).send({ topics });
-  });
+  collectTopics()
+    .then((topics) => {
+      res.status(200).send({ topics });
+    })
+    .then((err) => {
+      next(err);
+    });
 };
