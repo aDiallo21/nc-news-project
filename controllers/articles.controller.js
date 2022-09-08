10b23e064242cb6d83d103a6e6fa9e54bfcc371f
adiallo21 <abdoulaye32397@gmail.com>
2022-09-08 10:47:16 +0100

    patch votes and handle errors

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,10 +1,22 @@
-const { findArticleById } = require("../models/articles.model");
+const { findArticleById, incVotesById } = require("../models/articles.model");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
   return findArticleById(article_id)
     .then((article) => {
       res.status(200).send({ article: article });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.patchVotes = (req, res, next) => {
+  const { article_id } = req.params;
+  const { incVotes } = req.body;
+  return incVotesById(incVotes, article_id)
+    .then((updatedArticle) => {
+      res.status(201).send({ article: updatedArticle });
     })
     .catch((err) => {
       next(err);
--- models/articles.model.js
+++ models/articles.model.js
@@ -12,3 +12,19 @@ exports.findArticleById = (article_id) => {
     return result.rows[0];
   });
 };
+exports.incVotesById = (incVotes, article_id) => {
+  return db
+    .query(
+      "UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;",
+      [incVotes, article_id]
+    )
+    .then((result) => {
+      if (!result.rows.length) {
+        return Promise.reject({
+          status: 404,
+          msg: `Article id ${article_id} does not exist`,
+        });
+      }
+      return result.rows[0];
+    });
+};
